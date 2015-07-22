98366108e2cd49b81e494591620e9f043e08e2af
Radosław Włodkowski <radoslaw@wlodkowski.net>
2015-07-22 23:28:13 +0200

    Cleanup build script from unused SWC libraries paths

--- build.sh
+++ build.sh
@@ -4,32 +4,28 @@
 
 COMMON_OPT="-library-path+=./assets/assets.swc \
             -library-path+=./lib/OSMF/OSMF.swc \
-    -optimize=true \
-    -use-direct-blit=false \
-    -incremental=false \
-    -default-background-color=0x000000 \
-    -static-link-runtime-shared-libraries=true \
-    -define=CONFIG::FLASH_10_1,true"
+            -optimize=true \
+            -use-direct-blit=false \
+            -incremental=false \
+            -default-background-color=0x000000 \
+            -static-link-runtime-shared-libraries=true \
+            -define=CONFIG::FLASH_10_1,true"
 
-RELEASE_OPT="-library-path+=./lib/flashls/bin/release/flashlsOSMF.swc \
-             -debug=false \
+RELEASE_OPT="-debug=false \
              -define=CONFIG::LOGGING,false \
              -define=CONFIG::DEBUG,false"
     
-DEBUG_OPT="-library-path+=./lib/flashls/bin/debug/flashlsOSMF.swc \
-           -debug=true \
+DEBUG_OPT="-debug=true \
            -define=CONFIG::LOGGING,true \
            -define=CONFIG::DEBUG,true"
 
 # For latest release of Adobe Flash for Linux
 TARGET_10_2_OPT="-target-player=10.2 \
-                 -swf-version=11 \
-                 -library-path+=./lib/MSAdaptiveStreamingPlugin/1.0.10/MSAdaptiveStreamingPlugin-v1.0.10-osmf2.0.swc"
+                 -swf-version=11"
 
 # For newest Adobe Flash
 TARGET_11_4_OPT="-target-player=11.4 \
-                 -swf-version=17 \
-                 -library-path+=./lib/MSAdaptiveStreamingPlugin/1.0.12/MSAdaptiveStreamingPlugin-v1.0.12-osmf2.0.swc"
+                 -swf-version=17"
 
 echo "=================================="
 echo
